ac3ecf966acd4c6488866f3b0ec1a0d88ef91884
Aderibigbe Michael Oloruntomiwa <moloruntomiwa31@gmail.com>
2024-04-01 02:15:29 +0100

    Renamed Tutorial file, learnt about include and require functions, initialised files for Pizza Project

---
+++ templates/footer.php
@@ -0,0 +1,2 @@
+<p class="text-center text-secondary">Copyright &copy;2024</p>
+</body>
---
+++ templates/header.php
@@ -0,0 +1,35 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja's Pizza</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #EEEEEE;
+        }
+
+        .link {
+            text-decoration: none;
+        }
+
+        .bg-secondqry {
+            background: #DDDDDD !important;
+        }
+
+        .link-btn {
+            background-color: #76885B;
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="bg-secondary">
+        <div class="container d-flex align-items-center justify-content-between">
+            <h1 class="text-light">Ninja's Pizza</h1>
+            <ul class="d-flex list-unstyled align-items-center my-4">
+                <li class="mx-2"><a href="index.php" class="text-light link text-decoration-none">Home</a></li>
+                <li class="mx-2"><a href="add.php" class="text-light link-btn text-decoration-none rounded">Add a Pizza</a></li>
+            </ul>
+        </div>
+    </nav>
